4b7256a95fdb857eebab640a06fb5187f6277ea0
Zafar <jafarimam775@gmail.com>
2026-08-15 16:11:18 +0530

    Array to linked list conversion

---
+++ LinkedList/convertArr2LinkedList.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1,Node* next1){
+        data = data1;
+        next = next1;
+    }
+    public:
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+Node* convertarr2ll(vector<int>&arr){
+    Node* head = new Node(arr[0]); // Head Starting
+    Node* mover = head; 
+
+    for(int i=1; i<arr.size(); i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = temp; //
+    }
+    return head;
+}
+int main(){
+    vector<int>v = {23,1,83,51};
+    Node* head = convertarr2ll(v);
+    cout<<head->data;
+    return 0;
+}
