696e5b7f37749d2f7c6e5e338c963f3965c47b8c
Zafar <jafarimam775@gmail.com>
2026-09-15 13:34:35 +0530

    signly linked list insertion at end

--- LinkedList/insertAtEnd.cpp
+++ LinkedList/insertAtEnd.cpp
@@ -1,20 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Node{
+public:
     int data;
     Node* next;
 
     Node(int x){
         data = x;
         next = nullptr;
-
     }
-
 };
 Node* insertatEnd(Node* head, int x){
     Node* newNode = new Node(x);
     if(head == nullptr){
-        return nullptr;
+        return newNode;
     }
     Node* last = head;
     while(last->next != nullptr){
@@ -23,18 +22,22 @@ Node* insertatEnd(Node* head, int x){
     last->next = newNode;
     return head;
 }
-void PrintList(Node* head){
-    while(Node != nullptr){
-        cout << node->data << " ";
-        node = node->next;
+void PrintList(Node* &head){
+    Node* temp = head;
+    while(temp != nullptr){
+        cout << temp->data << " ";
+        temp = temp->next;
     }
+    cout<<endl;
 
 }
 int main(){
-    Node* Linkedlist<int> l = {1,3,4,2};
-    Node* head = next;
-    int a = 7;
-    insertatEnd(head,a);
-    PrintList(head);
+    Node* node1 = new Node(2);
+    node1->next = new Node(4);
+    node1->next->next = new Node(7);
+    int a;
+    cin >> a;
+    node1 = insertatEnd(node1,a);
+    PrintList(node1);
     return 0;
 }
